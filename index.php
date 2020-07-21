97d8c1a669a05e95f50ec2e07f2154584589503b
Jules <jules.chabaud@gmail.com>
2020-07-21 16:22:30 +0200

    Start directory - JULES CHABAUD && CELIA BELLOD

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php//Le répertoire de départ
+if($url == FALSE){
+  echo "Vous n'avez pas accès au dossier";
+} else {
+  $start_dir = "start";
+  $path_start = getcwd() . DIRECTORY_SEPARATOR . $start_dir;
+  if(in_array($start_dir, $current_dir)){
+    chdir($path_start);
+    $dir_start = scandir(getcwd());
+    print_r($dir_start);
+
+  } else {
+    mkdir($path_start);
+    chdir($path_start);
+    $dir_start =scandir(getcwd());
+    print_r($dir_start);
+  }
+}
+?>
