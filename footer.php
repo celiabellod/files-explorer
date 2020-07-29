906575a64bb0593b1f95afcb0a65e7bd6438349b
celia <celiabellod@icloud.com>
2020-07-29 07:45:25 +0200

    add javascript for open modal, and open and read file - CELIA BELLOD

--- footer.php
+++ footer.php
@@ -1,3 +1,57 @@
-  <script src="assets/js/main.js" type="text/javascript"></script>
+
+  <div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="closet">&times;</span>
+    <?php
+    if(isset($_GET['open'])) {
+      $file = $_GET['open'];
+      echo $file;
+      $ressource = fopen( $file, 'rb');
+      echo fread($ressource, filesize( $file));
+    }
+     ?>
+  </div>
+
+</div>
+
+  <script >
+
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var openModal = document.getElementsByClassName("openModal")[0];
+
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("closet")[0];
+
+ if(window.location.href.search("open=") != -1){
+      modal.style.display = "block";
+  }
+
+
+  /*
+  // When the user clicks the button, open the modal
+  openModal.onclick = function() {
+
+  modal.style.display = "block";
+}*/
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  </script>
+
   </body>
 </html>
