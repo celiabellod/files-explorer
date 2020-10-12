34509f100fd8438219c3b73a605d33667610e494
celia <celiabellod@icloud.com>
2020-10-12 19:16:27 +0200

    delete comments

--- class/Functionality.php
+++ class/Functionality.php
@@ -68,28 +68,4 @@ class Functionality {
   }
 
 
-
-  /*
-  foreach ($startArray as $value) {
-    $file = $value;
-    $hideFile = hide($file);
-    $keyHideFile = array_keys($hideFile);
-    $fileHideShow = show($hideFile);
-  }
-    function hide($file) {
-      $fileArray = explode(" ", $file);
-       foreach ($fileArray as $key => $value) {
-         $key = ' ';
-          $fileArray = [
-            $key => $value
-          ];
-          return $fileArray;
-      }
-    }
-    function show($hideFile) {
-      foreach ($hideFile as $value) {
-        echo $value;
-      }
-    }
-  */
 }
